07181e78521a48dd787e0698a44fe7dc60811f9f
Nihat Karaoglu <nkaraoglu@sophiagenetics.com>
2020-04-22 16:19:55 +0200

    101

--- wrapper.py
+++ wrapper.py
@@ -1,7 +1,7 @@
 import requests
-my_version = "1.00"
+my_version = "1.01"
 my_url = "https://raw.githubusercontent.com/sophia-genetics/uploader/master/wrapper.py"
-
+my_name = "wrapper2.py"
 def get_remote_version():
     url = "https://raw.githubusercontent.com/sophia-genetics/uploader/master/version.txt"
     version_file = requests.get(url)
@@ -9,9 +9,9 @@ def get_remote_version():
     return version
 
 def update_self():
-    requests.get(my_url)
-    me = open(__file__, 'wb')
-    me.write()
+    new_self = requests.get(my_url)
+    me = open(my_name, 'wb')
+    me.write(new_self.content)
     me.close()
 
 
